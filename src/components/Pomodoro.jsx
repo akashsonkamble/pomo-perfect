38a52e8a4bed03e268f305fcc3668f877c60e559
akash <akashsonkamble199@gmail.com>
2023-09-20 19:53:58 +0530

    The pomodoro timer added

---
+++ src/components/Pomodoro.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+
+const Pomodoro = () => {
+  const [timer, setTimer] = useState(25 * 60);
+  const [breakTimer, setBreakTimer] = useState(5 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  useEffect(() => {
+    if (timer === 0) {
+      setIsRunning(false);
+      setBreakTimer(5 * 60);
+    }
+  }, [timer]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  const startHandler = () => {
+    setIsRunning(true);
+  };
+
+  const pauseHandler = () => {
+    setIsRunning(false);
+  };
+
+  const resetHandler = () => {
+    setIsRunning(false);
+    setTimer(25 * 60);
+    setBreakTimer(0);
+  };
+
+  return (
+    <>
+      <div className=" text-center flex flex-col items-center align-center justify-center my-10 py-10">
+        <div className="text-9xl text-rebeccapurple">{formatTime(timer)}</div>
+        <div className="flex gap-10 mt-10">
+          <button
+            className="text-2xl text-rebeccapurple bg-primary border-primary border-2 hover:text-secondary hover:border-secondary hover:bg-white focus:outline-none  font-bold py-2 px-4 rounded"
+            onClick={startHandler}
+          >
+            Start
+          </button>
+          <button
+            className="text-2xl text-rebeccapurple bg-primary border-primary border-2 hover:text-secondary hover:border-secondary hover:bg-white focus:outline-none  font-bold py-2 px-4 rounded"
+            onClick={pauseHandler}
+          >
+            Pause
+          </button>
+          <button
+            className="text-2xl text-rebeccapurple bg-primary border-primary border-2 hover:text-secondary hover:border-secondary hover:bg-white focus:outline-none  font-bold py-2 px-4 rounded"
+            onClick={resetHandler}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+
+      {timer === 0 && (
+        <div className="flex flex-col items-center mt-8">
+          <h1 className="text-2xl font-bold mb-4 text-rebeccapurple">Break Timer</h1>
+          <div className="text-4xl mb-4 text-rebeccapurple">{formatTime(breakTimer)}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Pomodoro;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary: "#1C1936",
+      secondary: "#544f86",
+      rebeccapurple: "#663399",
+      white: "#ffffff",
+    },
     extend: {},
   },
   plugins: [],
