d8da0e8316543798af41150ffbff1fc180e987a5
akash <akashsonkamble199@gmail.com>
2023-09-21 13:07:41 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from './pages/Root';
+import RootLayout from "./pages/Root";
 import Login from "./pages/Login";
-// import Pomodoro from "./components/Pomodoro";
 import Register from "./pages/Register";
 import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
 
 const router = createBrowserRouter([
   {
@@ -13,28 +13,13 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Login /> },
       { path: "/register", element: <Register /> },
+      { path: "home", element: <HomePage /> },
     ],
   },
 ]);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-      {/* <Login />
-      <Register />
-      <section className="bg-Rebeccapurple text-white flex justify-between justify-items-center p-9 max-w-9xl h-20 items-center box-border m-auto">
-        <h1 className="font-bold text-4xl">PomoPerfect</h1>
-        <a className="text-2xl text-white" href="/logout">
-          Logout
-        </a>
-      </section>
-
-      <section className="justify-between justify-items-center p-4 max-w-4xl items-center box-border mx-auto">
-        <Pomodoro />
-      </section> */}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
